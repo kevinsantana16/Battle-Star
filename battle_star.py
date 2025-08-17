ae0ff341b31f4d9086b18fb13674d45fcd06b9e1
kevinsantana16 <paulofaust45@gmail.com>
2025-08-17 15:53:53 -0300

    Segundo commit: criei a class Player

--- battle_star.py
+++ battle_star.py
@@ -8,8 +8,7 @@ class Game:
         pygame.init()
 
         self.width, self.height = 1200, 1000
-        self.game_over = False  
-
+    
         # Database
         self.db = DBManager()
         self.high_score = self.db.get_high_score()
@@ -22,11 +21,14 @@ class Game:
         # Screen    
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.background = pygame.image.load("assets/background_3.png")
-       
-         # Player
-        self.player_image = pygame.image.load("assets/nave_111x111_2.png").convert()
-        self.player_image.set_colorkey((0, 0, 0))
-        self.player_rect = self.player_image.get_rect(center=(600, 700))
+
+        # Game Over
+        self.game_over = False
+
+        # Player
+        self.player = Player(self.width, self.height)
+        self.player_image = self.player.image
+        self.player_rect = self.player.rect
 
         # Enemies
         self.enemy_image = pygame.image.load("assets/rocha_3.png").convert()
@@ -41,35 +43,24 @@ class Game:
 
     def enemy_movement(self, enemy_rect):
         enemy_rect.y += self.enemy_speed
+
         if enemy_rect.top > self.height:
             enemy_rect.x = random.randint(0, self.width - enemy_rect.width)
             enemy_rect.y = -enemy_rect.height
             self.score += 1
 
         if enemy_rect.collidepoint(self.player_rect.center):
-           if self.score > self.high_score:
-            self.high_score = self.score
-            self.db.save_score(self.high_score)
-           self.game_over = True
-
-    def player_movement(self):
-        keys = pygame.key.get_pressed()
-        player_speed = 15
-
-        if keys[pygame.K_a] and self.player_rect.left > 0:
-            self.player_rect.x -= player_speed
-        if keys[pygame.K_d] and self.player_rect.right < self.width:
-            self.player_rect.x += player_speed
-        if keys[pygame.K_w] and self.player_rect.top > 0:
-            self.player_rect.y -= player_speed
-        if keys[pygame.K_s] and self.player_rect.bottom < self.height:
-            self.player_rect.y += player_speed
-
+            if self.score > self.high_score:
+                self.high_score = self.score
+                self.db.save_score(self.high_score)
+            self.game_over = True
+          
+    
     def reset_game(self):
         """Reinicia o jogo"""
         self.score = 0
         self.enemy_speed = 10
-        self.player_rect.center = (600, 700)
+        self.player.rect.center = (600, 700)
         self.enemy_rect.topleft = (random.randint(0, self.width - self.enemy_image.get_width()), 50)
         self.enemy_rect_2.topleft = (random.randint(0, self.width - self.enemy_image.get_width()), 50)
         self.game_over = False
@@ -90,7 +81,7 @@ class Game:
 
         pygame.display.flip()
 
-        # Espera por ação do jogador
+         # Espera por ação do jogador
         while self.game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -105,41 +96,66 @@ class Game:
                         self.db.close()
                         pygame.quit()
                         sys.exit()
-
-def run(self):
-    while True:
+    
+    def run(self):
+    
+       while True:
         
-        # Processa eventos antes do movimento
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.db.close()
-                pygame.quit()
-                sys.exit()
+           # Processa eventos antes do movimento
+           for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                   self.db.close()
+                   pygame.quit()
+                   sys.exit()
 
-        # Movimento só no jogo ativo
-        if not self.game_over:
-            self.player_movement()
-            self.enemy_speed += 0.01
-            self.enemy_movement(self.enemy_rect)
-            self.enemy_movement(self.enemy_rect_2)
+           # Movimento só no jogo ativo
+           if not self.game_over:
+               self.player.player_movement()
+               self.enemy_speed += 0.01
+               self.enemy_movement(self.enemy_rect)
+               self.enemy_movement(self.enemy_rect_2)
         
         
-        if self.game_over:
-            self.show_game_over_screen()
+           if self.game_over:
+                self.show_game_over_screen()
 
-        score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
+           score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
 
   
 
-        self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.player_image, self.player_rect)
-        self.screen.blit(self.enemy_image, self.enemy_rect)
-        self.screen.blit(self.enemy_image, self.enemy_rect_2)
-        self.screen.blit(score_text, (10, 10))
+           self.screen.blit(self.background, (0, 0))
+           self.screen.blit(self.player_image, self.player_rect)
+           self.screen.blit(self.enemy_image, self.enemy_rect)
+           self.screen.blit(self.enemy_image, self.enemy_rect_2)
+           self.screen.blit(score_text, (10, 10))
 
-        pygame.display.flip()
-        self.clock.tick(60)
+           pygame.display.flip()
+           self.clock.tick(60)
+
+
+class Player:
+    def __init__(self, screen_width, screen_height):
+        self.rect = pygame.Rect(600, 700, 111, 111)
+        self.image = pygame.image.load("assets/nave_111x111_2.png").convert()
+        self.image.set_colorkey((0, 0, 0))
+        self.width = screen_width
+        self.height = screen_height
+        self.player_speed = 15
+
+    def player_movement(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a] and self.rect.left > 0:
+            self.rect.x -= self.player_speed
+        if keys[pygame.K_d] and self.rect.right < self.width:
+            self.rect.x += self.player_speed
+        if keys[pygame.K_w] and self.rect.top > 0:
+            self.rect.y -= self.player_speed
+        if keys[pygame.K_s] and self.rect.bottom < self.height:
+            self.rect.y += self.player_speed
+
+
 
 if __name__ == "__main__":
     game = Game()
-    run(game)
+    game.run()
